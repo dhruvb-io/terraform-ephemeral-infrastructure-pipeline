92d8a328d0c9ba374e026e6a206b8bf8322d2714
iamdhruvbansal <dhruv.bansal0098@gmail.com>
2026-03-07 19:50:38 +0530

    Attempt06: Terraform With CICD

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ terraform {
     backend "s3" {
         bucket = "remote-backend-s3-dhruv"
         key = "terraform.tfstate"
-        region = var.region
+        region = "us-east-1"
         encrypt = true
     }
 }
